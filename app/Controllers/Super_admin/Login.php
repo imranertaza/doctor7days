1540933c6ef13b5c292b432b07566a49472eb226
Syed Imran Ertaza <imranertaza12@gmail.com>
2021-10-02 10:38:49 +0600

    decoreted made for super admin

---
+++ app/Controllers/Super_admin/Login.php
@@ -0,0 +1,272 @@
+<?php
+// ADEL CODEIGNITER 4 CRUD GENERATOR
+
+namespace App\Controllers\Super_admin;
+
+use App\Controllers\BaseController;
+
+use App\Models\Super_admin\LoginModel;
+
+class Login extends BaseController
+{
+    protected $loginModel;
+    protected $validation;
+    protected $session;
+
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        $this->loginModel = new LoginModel();
+        $this->validation =  \Config\Services::validation();
+        $this->session = \Config\Services::session();
+    }
+
+    /**
+     * Index Page for this controller.
+     */
+    public function index()
+    {
+        $this->isLoggedIn();
+    }
+    
+    /**
+     * This function used to check the user is logged in or not
+     */
+    function isLoggedIn()
+    {
+        $isLoggedInHospital = $this->session->isLoggedInHospital;
+        
+        if(!isset($isLoggedInHospital) || $isLoggedInHospital != TRUE)
+        {
+            echo view('Super_admin/Login/login');
+        }
+        else
+        {
+            return redirect()->to(site_url("/super_admin/dashboard"));
+        }
+        
+    }
+    
+    
+    /**
+     * This function used to logged in user
+     */
+    public function loginMe()
+    {
+
+        $session = \Config\Services::session();
+        $db = \Config\Database::connect();
+
+        $this->validation->setRule('email', 'Email', 'required|valid_email|max_length[128]|trim');
+        $this->validation->setRule('password', 'Password', 'required|max_length[32]');
+        
+        if($this->validation->withRequest($this->request)->run() == FALSE)
+        {
+            $this->index();
+        }
+        else
+        {
+            // $email = strtolower($this->security->xss_clean($this->input->post('email')));
+            $email = strtolower($this->request->getPost('email'));
+            $password = $this->request->getPost('password');
+
+            $result = $this->loginModel->loginMe($email, $password);
+            
+            if(!empty($result))
+            {
+
+                // Remember me (Remembering the user email and password) Start
+                if (!empty($this->request->getPost("remember"))) { 
+
+                    setcookie('login_email',$email,time()+ (86400 * 30), "/");
+                    setcookie('login_password',$password,time() + (86400 * 30), "/");
+                    
+                }else{
+                    if (isset($_COOKIE['login_email'])) {
+                        setcookie('login_email','', 0, "/");
+                    }
+                    if (isset($_COOKIE['login_password'])) {
+                        setcookie('login_password','', 0, "/");
+                    }
+                }
+                // Remember me (Remembering the user email and password) End
+
+
+
+                $license = $this->loginModel->licenseCheck($result->h_id);
+                
+                if ($license == true ) {
+                    $sessionArray = array(   
+                                        'h_Id'=>$result->h_id,                  
+                                        'hospitalName'=>$result->name,
+                                        'hospitalAdminRole'=> $result->role_id,
+                                        'isLoggedInHospital' => TRUE
+                                );
+                    
+                    $session->set($sessionArray);
+                    
+                    return redirect()->to(site_url("/super_admin/dashboard"));
+                }else{
+                    // License check and update Hospital status (start)
+                    $hospitalData = array(
+                        'status' => '0', 
+                    );
+                    $builder = $db->table('hospital');
+                    $builder->where('h_id', $result->h_id);
+                    $builder->update($hospitalData);
+                    // License check and update Hospital status (end)
+
+
+                    // License check and update users status (start)
+                    $userData = array(
+                        'status' => '0', 
+                        'updatedDtm' => date('Y-m-d h:i:s')
+                    );
+                    $builder = $db->table('users');
+                    $builder->where('h_id', $result->h_id);
+                    $builder->update($userData);
+                    // License check and update users status (start)
+
+                    $session->setFlashdata('error', 'Your License Is Expired');
+                    
+                    return redirect()->to(site_url("/super_admin/login"));
+                }
+                return redirect()->to(site_url("/super_admin/dashboard"));
+            }
+            else
+            {
+                $session->setFlashdata('error', 'Email or password mismatch');
+                $this->index();
+            }
+        }
+    }
+
+    /**
+     * This function used to load forgot password view
+     */
+    public function forgotPassword()
+    {
+        $isLoggedIn = $this->session->userdata(isLoggedIn);
+        
+        if(!isset($isLoggedIn) || $isLoggedIn != TRUE)
+        {
+            $this->load->view('forgotPassword');
+        }
+        else
+        {
+            redirect('/dashboard');
+        }
+    }
+
+    /**
+     * This function used to generate reset password request link
+     */
+
+    public function reset_link()
+    {
+        $email = $this->input->post('email',TRUE);
+
+        $result = $this->db->select('email')->get_where('users', array('email' => $email ))->num_rows();
+
+        if ($result > 0) {
+            $tokan =  rand(1000,9999);
+
+            $_SESSION['tokan']= $tokan;
+            $_SESSION['email'] = $email;
+
+            $message = "Please Click On Password Reset Link <br> <a href='".base_url('login/reset?tokan=').$tokan."'>Reset Password</a>";
+
+            //print $message;
+            $this->email->from('example@gmail.com', 'example');
+            $this->email->to($email);
+            $this->email->subject('Reset Password Link'); 
+            $this->email->message($message);
+            if (!$this->email->send()) {
+                show_error($this->email->print_debugger()); }
+            else {
+                echo 'Reset password link sent successfully!';
+            }
+
+        }else{            
+            $this->session->set_flashdata('message', '<div style="margin-top: 12px" class="alert alert-danger" id="message">Your Email Address Could Not Be Found</div>');
+                redirect(site_url('login/forgotPassword'));
+        }
+
+    }
+
+
+    /**
+     * This function used to load input forgot password view
+     */
+
+    public function reset()
+    {
+        $data['tokan'] = $this->input->get('tokan');
+        $_SESSION['lasttokan'] = $data['tokan'];        
+        if ($_SESSION['tokan'] != $data['tokan']) {
+            $this->session->set_flashdata('message', '<div style="margin-top: 12px" class="alert alert-danger" id="message">Invalid Tokan</div>');
+            redirect(site_url('login/forgotPassword'));
+        }else{
+            $this->load->view('login/resetpassword');
+        }
+    }
+
+
+    /**
+     * This function used to Update reset password 
+     */ 
+    public function update_password()
+    {
+        
+        $_SESSION['lasttokan'];
+        $_SESSION['tokan'];
+
+        if ($_SESSION['tokan'] == $_SESSION['lasttokan']) {
+
+            $password = $this->input->post('password',TRUE); 
+            $cpassword = $this->input->post('cpassword',TRUE);
+
+            if ($password == $cpassword) {
+                $this->db->where('email', $_SESSION['email']);
+                $this->db->update('users', array('password' => sha1($password)));
+                $this->session->set_flashdata('message', '<div style="margin-top: 12px" class="alert alert-success" id="message">Your Password Update Success</div>');
+                redirect(site_url('login'));
+            }else{
+                $this->session->set_flashdata('message', '<div style="margin-top: 12px" class="alert alert-danger" id="message">Password And Confirm Password Does Not Match</div>');
+                redirect(base_url('login/reset?tokan=').$_SESSION['tokan']);
+            }
+
+        }else{
+            $this->session->set_flashdata('message', '<div style="margin-top: 12px" class="alert alert-danger" id="message">Invalid Tokan</div>');
+            redirect(site_url('login/forgotPassword'));
+            
+        }
+
+    }
+    
+    
+    
+    /**
+     * This function used to logout 
+     */ 
+    
+    function logout()
+	 {
+        $session = \Config\Services::session();
+
+        unset($_SESSION['h_Id']);
+        unset($_SESSION['hospitalName']);
+        unset($_SESSION['isLoggedInHospital']);
+        unset($_SESSION['hospitalAdminRole']);
+
+        $session->destroy();
+        return redirect()->to('/super_admin/login');
+	 }
+
+
+    
+}
+
+?>
---
+++ app/Models/Super_admin/LoginModel.php
@@ -0,0 +1,172 @@
+<?php
+// ADEL CODEIGNITER 4 CRUD GENERATOR
+
+namespace App\Models\Hospital_admin;
+use CodeIgniter\Model;
+
+class LoginModel extends Model
+{
+    
+    
+    /**
+     * This function used to check the login credentials of the user
+     * @param string $email : This is email of the user
+     * @param string $password : This is encrypted password of the user
+     */
+    function loginMe($email, $password)
+    {
+
+        $db = \Config\Database::connect();
+        $builder = $db->table('users');
+        $builder->select('BaseTbl.email, BaseTbl.password, BaseTbl.name, BaseTbl.h_id, BaseTbl.role_id');
+        $builder->from('users as BaseTbl');
+        //$this->db->join('tbl_roles as Roles','Roles.roleId = BaseTbl.roleId');
+        $builder->where('BaseTbl.email', $email);
+        $builder->where('BaseTbl.status', 1);
+        $query = $builder->get();
+        
+        $user = $query->getRow();
+        
+        if(!empty($user)){
+            if(SHA1($password) == $user->password){
+                return $user;
+            } else {
+                return array();
+            }
+        } else {
+            return array();
+        }
+    }
+
+    function licenseCheck($h_Id){
+        $db = \Config\Database::connect();
+        $builder = $db->table('license');
+        $query = $builder->select('end_date')->where('h_id',$h_Id)->get();
+        
+        $today = date('Y-m-d');
+        if ($query->getRow()->end_date > $today) {
+            $data = true;
+        }else{
+            $data = false;
+        }
+        return $data;
+    }
+    
+    
+   //  private function confirmRole($roleId){
+            // if ($roleId == 2) { $usrTable = 'users'; $usrId = 'sch_id'; }
+   //   if ($roleId == 4) { $usrTable = 'teacher'; $usrId = 'teacher_id'; }
+   //   if ($roleId == 5) { $usrTable = 'librarian'; $usrId = 'librarian_id'; }
+   //   if ($roleId == 6) { $usrTable = 'parents'; $usrId = 'parent_id'; }
+   //   if ($roleId == 7) { $usrTable = 'student'; $usrId = 'student_id'; }
+   //   if ($roleId == 8) { $usrTable = 'accountant'; $usrId = 'accountant_id'; }
+        
+   //   $tableInfo = array("usrTable"=>$usrTable, "usrId"=>$usrId);
+   //   return $tableInfo;
+   //  }
+
+    /**
+     * This function used to check email exists or not
+     * @param {string} $email : This is users email id
+     * @return {boolean} $result : TRUE/FALSE
+     */
+    function checkEmailExist($email)
+    {
+        $this->db->select('userId');
+        $this->db->where('email', $email);
+        $this->db->where('isDeleted', 0);
+        $query = $this->db->get('tbl_users');
+
+        if ($query->num_rows() > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    /**
+     * This function used to insert reset password data
+     * @param {array} $data : This is reset password data
+     * @return {boolean} $result : TRUE/FALSE
+     */
+    function resetPasswordUser($data)
+    {
+        $result = $this->db->insert('tbl_reset_password', $data);
+
+        if($result) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
+
+    /**
+     * This function is used to get customer information by email-id for forget password email
+     * @param string $email : Email id of customer
+     * @return object $result : Information of customer
+     */
+    function getCustomerInfoByEmail($email)
+    {
+        $this->db->select('userId, email, name');
+        $this->db->from('tbl_users');
+        $this->db->where('isDeleted', 0);
+        $this->db->where('email', $email);
+        $query = $this->db->get();
+
+        return $query->row();
+    }
+
+    /**
+     * This function used to check correct activation deatails for forget password.
+     * @param string $email : Email id of user
+     * @param string $activation_id : This is activation string
+     */
+    function checkActivationDetails($email, $activation_id)
+    {
+        $this->db->select('id');
+        $this->db->from('tbl_reset_password');
+        $this->db->where('email', $email);
+        $this->db->where('activation_id', $activation_id);
+        $query = $this->db->get();
+        return $query->num_rows();
+    }
+
+    // This function used to create new password by reset link
+    function createPasswordUser($email, $password)
+    {
+        $this->db->where('email', $email);
+        $this->db->where('isDeleted', 0);
+        $this->db->update('tbl_users', array('password'=>getHashedPassword($password)));
+        $this->db->delete('tbl_reset_password', array('email'=>$email));
+    }
+
+    /**
+     * This function used to save login information of user
+     * @param array $loginInfo : This is users login information
+     */
+    function lastLogin($loginInfo)
+    {
+        $this->db->trans_start();
+        $this->db->insert('tbl_last_login', $loginInfo);
+        $this->db->trans_complete();
+    }
+
+    /**
+     * This function is used to get last login info by user id
+     * @param number $userId : This is user id
+     * @return number $result : This is query result
+     */
+    function lastLoginInfo($userId)
+    {
+        $this->db->select('BaseTbl.createdDtm');
+        $this->db->where('BaseTbl.user_id', $userId);
+        $this->db->order_by('BaseTbl.id', 'DESC');
+        $this->db->limit(1);
+        $query = $this->db->get('tbl_last_login as BaseTbl');
+
+        return $query->row();
+    }
+}
+
+?>
