b6a5a11ccf7531cbf5d326609dc928aad9e34467
Syed Imran Ertaza <imranertaza12@gmail.com>
2022-01-26 11:23:45 +0600

    fixed some errors.

--- app/Helpers/Global_helper.php
+++ app/Helpers/Global_helper.php
@@ -1131,7 +1131,8 @@ function subCatSaleInOption($categoryId)
 
 }
 
-function division()
+
+function divisionView($selected = 0)
 {
     $divisions = array(
         array('id' => '1', 'name' => 'Chattagram', 'bn_name' => 'চট্টগ্রাম', 'url' => 'www.chittagongdiv.gov.bd'),
@@ -1165,7 +1166,16 @@ function divisionView($selected = 0)
 
 function divisionname($id)
 {
-    $divisions = division();
+    $divisions = array(
+        array('id' => '1', 'name' => 'Chattagram', 'bn_name' => 'চট্টগ্রাম', 'url' => 'www.chittagongdiv.gov.bd'),
+        array('id' => '2', 'name' => 'Rajshahi', 'bn_name' => 'রাজশাহী', 'url' => 'www.rajshahidiv.gov.bd'),
+        array('id' => '3', 'name' => 'Khulna', 'bn_name' => 'খুলনা', 'url' => 'www.khulnadiv.gov.bd'),
+        array('id' => '4', 'name' => 'Barisal', 'bn_name' => 'বরিশাল', 'url' => 'www.barisaldiv.gov.bd'),
+        array('id' => '5', 'name' => 'Sylhet', 'bn_name' => 'সিলেট', 'url' => 'www.sylhetdiv.gov.bd'),
+        array('id' => '6', 'name' => 'Dhaka', 'bn_name' => 'ঢাকা', 'url' => 'www.dhakadiv.gov.bd'),
+        array('id' => '7', 'name' => 'Rangpur', 'bn_name' => 'রংপুর', 'url' => 'www.rangpurdiv.gov.bd'),
+        array('id' => '8', 'name' => 'Mymensingh', 'bn_name' => 'ময়মনসিংহ', 'url' => 'www.mymensinghdiv.gov.bd')
+    );
 
     $row = '';
     foreach ($divisions as $rows) {
@@ -1174,6 +1184,24 @@ function divisionname($id)
         }
     }
     return $row;
+}
+
+
+
+function division()
+{
+    $divisions = array(
+        array('id' => '1', 'name' => 'Chattagram', 'bn_name' => 'চট্টগ্রাম', 'url' => 'www.chittagongdiv.gov.bd'),
+        array('id' => '2', 'name' => 'Rajshahi', 'bn_name' => 'রাজশাহী', 'url' => 'www.rajshahidiv.gov.bd'),
+        array('id' => '3', 'name' => 'Khulna', 'bn_name' => 'খুলনা', 'url' => 'www.khulnadiv.gov.bd'),
+        array('id' => '4', 'name' => 'Barisal', 'bn_name' => 'বরিশাল', 'url' => 'www.barisaldiv.gov.bd'),
+        array('id' => '5', 'name' => 'Sylhet', 'bn_name' => 'সিলেট', 'url' => 'www.sylhetdiv.gov.bd'),
+        array('id' => '6', 'name' => 'Dhaka', 'bn_name' => 'ঢাকা', 'url' => 'www.dhakadiv.gov.bd'),
+        array('id' => '7', 'name' => 'Rangpur', 'bn_name' => 'রংপুর', 'url' => 'www.rangpurdiv.gov.bd'),
+        array('id' => '8', 'name' => 'Mymensingh', 'bn_name' => 'ময়মনসিংহ', 'url' => 'www.mymensinghdiv.gov.bd')
+    );
+
+    return $divisions;
 }
 
 function districtView()
@@ -2216,7 +2244,7 @@ function id_by_global_address($id)
             "division" => 0,
             "zila" => 0,
             "upazila" => 0
-        );
+            );
     }
 
     return $view;
